aa27e68995d2ebc076e173371fbcbfce1bf0258a
katesim <sebastia256@gmail.com>
2023-02-16 20:42:24 -0800

    add hit handler and dto

---
+++ stats_server/src/main/java/hit/HitController.java
@@ -0,0 +1,23 @@
+package hit;
+
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class HitController {
+
+    @PostMapping
+    public ResponseEntity<Object> create(@Validated @RequestBody HitDto hitDto) {
+        log.info(hitDto.toString());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
---
+++ stats_server/src/main/java/hit/HitDto.java
@@ -0,0 +1,18 @@
+package hit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+public class HitDto {
+    @NotBlank
+    private String app;
+    @NotBlank
+    private String uri;
+    @NotBlank
+    private String ip;
+    private String timestamp;
+}
