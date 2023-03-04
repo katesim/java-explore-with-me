2703a4155f81d779d36bc2fd6d746b062e6585ed
katesim <sebastia256@gmail.com>
2023-03-03 20:39:32 -0800

    fix error message for missing required params

--- main_server/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
+++ main_server/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
@@ -11,6 +11,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -44,7 +45,8 @@ public class ErrorHandler {
             ValidationException.class,
             MethodArgumentNotValidException.class,
             IllegalArgumentException.class,
-            MethodArgumentTypeMismatchException.class
+            MethodArgumentTypeMismatchException.class,
+            MissingServletRequestParameterException.class
     })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleValidationException(final Throwable exc) {
